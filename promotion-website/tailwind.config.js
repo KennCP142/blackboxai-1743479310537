54d7764b82e0e5c7dcbc2f156a352b1faa49fcdc
KennCP142 <c14220001@john.petra.ac.id>
2025-04-01 10:59:44 +0700

    Add promotion-website/tailwind.config.js 1743479983703

---
+++ promotion-website/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        secondary: '#1e40af',
+      },
+    },
+  },
+  plugins: [],
+}
